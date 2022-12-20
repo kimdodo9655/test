0139d79f37c63dd1f657a820d492c046e864f93d
kimdodo9655 <kimdodo9655@gmail.com>
2022-12-20 14:26:13 +0900

    add test

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+echo '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>다양한 선택자를 지정하는 방법</p>
+</body>
+</html>';
+?>
